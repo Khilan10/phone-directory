bd3778dc3ae2d0d3ac7aff44cb0f85bfbd33277d
Khilan10 <mahajankhilan1@gmail.com>
2020-01-12 20:34:50 +0530

    - Adding Style to home page

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div class="component-body-container ">
       <Header />
-      <button>Add</button>
-      <div>
-        <span>Name</span><br />
-        <span>Phone</span>
+      <button className="custom-btn  btn-add">Add</button>
+      <div className="grid-container heading-container">
+        <span className="grid-item name-heading">Name</span>
+        <span className="grid-item phone-heading">Phone</span>
       </div>
     </div>
   );
